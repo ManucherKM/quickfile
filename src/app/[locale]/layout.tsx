fbad20c43f865e623e5ef4d6282064be8b74bf7c
ManucherKM <manucher20056@gmail.com>
2023-12-13 20:33:37 +0500

    add YM

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import { Metadata } from 'next'
 import { NextIntlClientProvider, useMessages } from 'next-intl'
 import { getTranslations } from 'next-intl/server'
 import { notFound } from 'next/navigation'
+import Script from 'next/script'
 import { ReactNode } from 'react'
 
 export interface IGenerateMetadata {
@@ -39,6 +40,22 @@ export default function LocaleLayout({
 
 	return (
 		<html lang={locale}>
+			<Script>
+				{/* <!-- Yandex.Metrika counter --> */}
+				{`  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();
+for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(95870160, "init", {
+	 clickmap:true,
+	 trackLinks:true,
+	 accurateTrackBounce:true,
+	 webvisor:true
+});
+`}
+			</Script>
 			<body>
 				<NextIntlClientProvider locale={locale} messages={messages}>
 					<LoaderProvider>
