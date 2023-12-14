2cc08c47873b592adee0b69f1cc4afaf5c752811
ManucherKM <manucher20056@gmail.com>
2023-12-14 22:07:04 +0500

    fix bug

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 const withNextIntl = require('next-intl/plugin')();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        const headers = [];
+        if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+            headers.push({
+                headers: [
+                    {
+                        key: 'X-Robots-Tag',
+                        value: 'noindex',
+                    },
+                ],
+                source: '/:path*',
+            });
+        }
+        return headers;
+    },
+}
 
 module.exports = withNextIntl(nextConfig)
