c73c84183ee9e6ecb68b5c3ad2331142790d1d14
ManucherKM <manucher20056@gmail.com>
2023-12-20 13:26:14 +0500

    add robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,18 @@
+import { env } from '@/config/env'
+import { availableLocales } from '@/locale'
+import { MetadataRoute } from 'next'
+
+const CLIENT_URL = env.get('CLIENT_URL').required().asString()
+
+export default function robots(): MetadataRoute.Robots {
+	const disallowLocale = availableLocales.map(locale => `${locale}/*`)
+
+	return {
+		rules: {
+			userAgent: '*',
+			allow: '/',
+			disallow: [...disallowLocale],
+		},
+		sitemap: CLIENT_URL + '/sitemap.xml',
+	}
+}
