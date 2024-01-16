f7b9c7ee226336a3527a5c1db85e1b9a2ac161b2
ManucherKM <manucher20056@gmail.com>
2024-01-16 23:21:24 +0500

    add ru locale

--- src/app/[locale]/policy/page.tsx
+++ src/app/[locale]/policy/page.tsx
@@ -1,104 +1,110 @@
 import { NavBarBack } from '@/components'
 import clsx from 'clsx'
 import { Paragraph, Title, Ul } from 'kuui-react'
+import { useTranslations } from 'next-intl'
 import classes from './Policy.module.scss'
 
 function Policy() {
+	const t = useTranslations()
+
 	const styles = clsx(['container', classes.root])
 	return (
 		<>
 			<NavBarBack />
 			<div className={styles}>
-				<Title>Общие положения</Title>
+				<Title>{t('general_provisions')}</Title>
 				<Paragraph>
-					Данный раздел политики использования (далее - Политика) определяет
-					условия использования сервиса QuickFile.
+					{t(
+						'this_section_of_the_usage_policy_hereinafter_referred_to_as_the_policy_defines_the_terms_and_conditions_of_use_of_the_quickfile_service',
+					)}
 				</Paragraph>
-				<Title>Определения</Title>
+				<Title>{t('definitions')}</Title>
 				<Paragraph>
-					В настоящей Политике используются следующие термины:
-				</Paragraph>
-				<Ul
-					items={[
-						{
-							children: (
-								<Paragraph>
-									Сервис - программное обеспечение, предоставляющее услуги по
-									обмену и хранению файлов.
-								</Paragraph>
-							),
-						},
-						{
-							children: (
-								<Paragraph>
-									Пользователь - физическое или юридическое лицо, использующее
-									сервис.
-								</Paragraph>
-							),
-						},
-						{
-							children: (
-								<Paragraph>
-									Администрация - лица, осуществляющие управление сервисом.
-								</Paragraph>
-							),
-						},
-					]}
-				/>
-				<Title>Ответственность за материалы</Title>
-				<Paragraph>
-					Администрация не несет ответственности за материалы, загружаемые,
-					отправляемые, полученные Пользователями посредством сервиса.
-					Пользователь несет полную ответственность за содержание загружаемых и
-					передаваемых им файлов.
-				</Paragraph>
-				<Paragraph>
-					В частности, Пользователь несет ответственность за соблюдение
-					следующих требований:
+					{t('the_following_terms_are_used_in_this_policy')}
 				</Paragraph>
 				<Ul
 					items={[
 						{
 							children: (
 								<Paragraph>
-									Содержание файлов не должно противоречить законодательству
-									страны пользователя.
+									{t(
+										'service__software_that_provides_file_sharing_and_storage_services',
+									)}
 								</Paragraph>
 							),
 						},
 						{
 							children: (
 								<Paragraph>
-									Файлы не должны содержать объекты интеллектуальной
-									собственности, копирование которых запрещено владельцами
-									оригинала.
+									{t('user__an_individual_or_legal_entity_using_the_service')}
 								</Paragraph>
 							),
 						},
 						{
 							children: (
 								<Paragraph>
-									Файлы не должны содержать вредоносные программы.
+									{t('administration__persons_who_manage_the_service')}
 								</Paragraph>
+							),
+						},
+					]}
+				/>
+				<Title>{t('responsibility_for_materials')}</Title>
+				<Paragraph>
+					{t(
+						'administration_is_not_responsible_for_materials_uploaded_sent_received_by_users_through_the_service_the_user_is_fully_responsible_for_the_content_of_files_uploaded_and_transmitted_by_him',
+					)}
+				</Paragraph>
+				<Paragraph>
+					{t(
+						'in_particular_the_user_is_responsible_for_compliance_with_the_following_requirements',
+					)}
+				</Paragraph>
+				<Ul
+					items={[
+						{
+							children: (
+								<Paragraph>
+									{t(
+										'the_content_of_the_files_must_not_contradict_the_legislation_of_the_user',
+									)}
+								</Paragraph>
+							),
+						},
+						{
+							children: (
+								<Paragraph>
+									{t(
+										'files_must_not_contain_intellectual_property_the_copying_of_which_is_prohibited_by_the_owners_of_the_original',
+									)}
+								</Paragraph>
+							),
+						},
+						{
+							children: (
+								<Paragraph>{t('the_files_must_not_contain_malware')}</Paragraph>
 							),
 						},
 					]}
 				/>
 
 				<Paragraph>
-					В случае нарушения Пользователем указанных требований Администрация
-					вправе удалить соответствующие файлы из сервиса, а также принять
-					необходимые меры для пресечения повторных нарушений Пользователем.
+					{t(
+						'if_the_user_violates_these_requirements_the_administration_has_the_right_to_remove_the_corresponding_files_from_the_service_as_well_as_to_take_the_necessary_measures_to_prevent_repeated_violations_by_the_user',
+					)}
 				</Paragraph>
 
-				<Title>Изменения</Title>
+				<Title>{t('alterations')}</Title>
 				<Paragraph>
-					Администрация оставляет за собой право вносить изменения в настоящую
-					Политику без уведомления Пользователей.
+					{t(
+						'the_administration_reserves_the_right_to_make_changes_to_this_policy_without_notifying_users',
+					)}
 				</Paragraph>
 
-				<Title>Срок действия</Title>
-				<Paragraph>Настоящая Политика действует бессрочно.</Paragraph>
+				<Title>{t('expiration_date')}</Title>
+				<Paragraph>
+					{t('this_policy_shall_remain_in_effect_indefinitely')}
+				</Paragraph>
 			</div>
 		</>
 	)
