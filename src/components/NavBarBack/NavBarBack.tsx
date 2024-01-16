710113cd531a54c7cfdb65ab832f297f30d5efef
ManucherKM <manucher20056@gmail.com>
2024-01-16 22:43:46 +0500

    add policy

--- src/components/NavBarBack/NavBarBack.tsx
+++ src/components/NavBarBack/NavBarBack.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import clsx from 'clsx'
 import { Subtitle } from 'kuui-react'
 import { useTranslations } from 'next-intl'
@@ -17,7 +19,23 @@ export const NavBarBack: FC = () => {
 				onClick={() => router.back()}
 				dimension="small"
 			>
-				&#x2190; {t('back')}
+				{/* &#x2190; */}
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="6"
+					height="10"
+					viewBox="0 0 7 12"
+					fill="none"
+				>
+					<path
+						d="M6 1L1 6L6 11"
+						stroke="currentColor"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					/>
+				</svg>
+				{t('back')}
 			</Subtitle>
 		</nav>
 	)
